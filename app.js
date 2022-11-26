ea28b8a39b2288072d6f9793744fc5cfc7bf675f
Phill <70281330+Phillipnewell@users.noreply.github.com>
2022-11-25 18:42:38 -0700

    in with liquid, out with pug

--- app.js
+++ app.js
@@ -1,51 +1,47 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
 //https://liquidjs.com/tutorials/use-in-expressjs.html
-var { Liquid } = require('liquidjs'); 
+var { Liquid } = require("liquidjs");
 var engine = new Liquid();
-//https://liquidjs.com/tutorials/use-in-expressjs.html
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+
+var indexRouter = require("./routes/index");
+var usersRouter = require("./routes/users");
 
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+// liquid engine setup
+app.engine("liquid", engine.express());
+app.set("views", "./views"); // specify the views directory
+app.set("view engine", "liquid"); // set liquid to default
+//https://liquidjs.com/tutorials/use-in-expressjs.html
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
-
-//https://liquidjs.com/tutorials/use-in-expressjs.html
-app.engine('liquid', engine.express()); 
-app.set('views', './views');            // specify the views directory
-app.set('view engine', 'liquid');       // set liquid to default
-//https://liquidjs.com/tutorials/use-in-expressjs.html
 
 module.exports = app;
