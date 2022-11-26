15024805ebf05d14d6622e35c92cafaa455be3c2
Phill <70281330+Phillipnewell@users.noreply.github.com>
2022-11-25 19:04:59 -0700

    routing with new users

--- routes/index.js
+++ routes/index.js
@@ -1,57 +1,69 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Express" });
 });
 
 module.exports = router;
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-const sqlite3 = require('sqlite3').verbose()
+const sqlite3 = require("sqlite3").verbose();
 //https://heynode.com/tutorial/how-validate-and-sanitize-expressjs-form/
 //const { check, validationResult } = require('express-validator');
 
 /* GET home page. */
-router.get('/', function (_req, res) {
-  var db = new sqlite3.Database('mydb.sqlite3', sqlite3.OPEN_READWRITE | sqlite3.OPEN_CREATE,
+router.get("/", function (_req, res) {
+  var db = new sqlite3.Database(
+    "mydb.sqlite3",
+    sqlite3.OPEN_READWRITE | sqlite3.OPEN_CREATE,
     (err) => {
       if (err) {
         console.log("Getting error " + err);
         exit(1);
       }
       //Query if the table exists if not lets create it on the fly!
-      db.all(`SELECT name FROM sqlite_master WHERE type='table' AND name='blog'`,
+      db.all(
+        `SELECT name FROM sqlite_master WHERE type='table' AND name='blog'`,
         (_err, rows) => {
           if (rows.length === 1) {
             console.log("Table exists!");
             db.all(` select blog_id, blog_txt from blog`, (_err, rows) => {
               console.log("returning " + rows.length + " records");
-              res.render('index', { title: 'Express', data: rows });
+              res.render("index", { title: "Express", data: rows });
             });
           } else {
             console.log("Creating table and inserting some sample data");
-            db.exec(`create table blog (
+            db.exec(
+              `CREATE TABLE blog (
                      blog_id INTEGER PRIMARY KEY AUTOINCREMENT,
+                     blog_title text NOT NULL,
                      blog_txt text NOT NULL);
 
-                      insert into blog (blog_txt)
-                      values ('This is a great blog'),
-                             ('Oh my goodness blogging is fun');`,
+                     insert into blog (blog_txt)
+                     values ('This is a great blog'),
+                     ('Oh my goodness blogging is fun');`,
               () => {
-                db.all(` select blog_id, blog_txt from blog`, (_err, rows) => {
-                  res.render('index', { title: 'Express', data: rows });
-                });
-              });
+                db.all(
+                  ` select blog_id, blog_title, blog_txt from blog`,
+                  (_err, rows) => {
+                    res.render("index", { title: "Express", data: rows });
+                  }
+                );
+              }
+            );
           }
-        });
-    });
+        }
+      );
+    }
+  );
 });
 
-router.post('/add', (req, res, _next) => {
+router.post("/add", (req, res, _next) => {
   console.log("Adding blog to table without sanitizing input! YOLO BABY!!");
-  var db = new sqlite3.Database('mydb.sqlite3',
+  var db = new sqlite3.Database(
+    "mydb.sqlite3",
     sqlite3.OPEN_READWRITE | sqlite3.OPEN_CREATE,
     (err) => {
       if (err) {
@@ -63,18 +75,19 @@ router.post('/add', (req, res, _next) => {
       //this is ripe for a exploit! DO NOT use this in production :)
       //Try and figure out how why this is unsafe and how to fix it.
       //HINT: the answer is in the XKCD comic on the home page little bobby tables :)
-      
+
       db.exec(`insert into blog ( blog_txt)
-                values ('${req.body.blog}');`)
+                values ('${req.body.blog}');`);
       //redirect to homepage
-      res.redirect('/');
+      res.redirect("/");
     }
   );
-})
+});
 
-router.post('/delete', (req, res, _next) => {
+router.post("/delete", (req, res, _next) => {
   console.log("deleting stuff without checking if it is valid! SEND IT!");
-  var db = new sqlite3.Database('mydb.sqlite3',
+  var db = new sqlite3.Database(
+    "mydb.sqlite3",
     sqlite3.OPEN_READWRITE | sqlite3.OPEN_CREATE,
     (err) => {
       if (err) {
@@ -90,10 +103,10 @@ router.post('/delete', (req, res, _next) => {
       // check('username', 'Username Must Be an Email Address').trim().escape()
       // const { check, validationResult } = require('express-validator');
 
-      db.exec(`delete from blog where blog_id='${req.body.blog}';`);     
-      res.redirect('/');
+      db.exec(`delete from blog where blog_id='${req.body.blog}';`);
+      res.redirect("/");
     }
   );
-})
+});
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,16 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get("/", function (req, res) {
+  res.send("respond with a resource");
 });
+
+/* GET user new form. */
+router.get("/new", function (req, res) {
+  res.send("respond with a resource");
+});
+
+const usersRouter = require("./routes/users");
 
 module.exports = router;
