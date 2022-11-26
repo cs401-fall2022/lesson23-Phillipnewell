e55eb64811bc043c76085f4bc1a5d5bdde3796d8
Phill <70281330+Phillipnewell@users.noreply.github.com>
2022-11-25 19:07:29 -0700

    nevermind its already implemented

--- routes/users.js
+++ routes/users.js
@@ -6,11 +6,4 @@ router.get("/", function (req, res) {
   res.send("respond with a resource");
 });
 
-/* GET user new form. */
-router.get("/new", function (req, res) {
-  res.send("respond with a resource");
-});
-
-const usersRouter = require("./routes/users");
-
 module.exports = router;
